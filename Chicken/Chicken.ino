6f2dc552c530f02dab7e77b58e41ce89d62cb5f0
ShanylOng <38065130+ShanylOng@users.noreply.github.com>
2018-04-04 17:13:47 +0800

    Add files via upload

---
+++ Chicken/Chicken.ino
@@ -0,0 +1,41 @@
+#include <motor.h>
+
+#include <Wire.h>
+
+#include <VL53L0X.h>
+
+#include <helper_3dmath.h>
+#include <MPU6050_6Axis_MotionApps20.h>
+
+#include <I2Cdev.h>
+
+MPU6050 gyro;
+Motor left(6,5), right(9,10);
+float origin = 0;
+float integral = 0;
+float lasterror = 0;
+float kp = 1;
+float ki = 1;
+float kd = 1;
+
+int speed = 50;
+
+void setup() {
+      Wire.begin();
+      gyro.start(-53,-1,96,988);
+}
+void loop() {
+// accepts float origin                
+      float angle = gyro.getangle();
+      float cfac = angle;
+      float setoff = origin - cfac;
+    
+      float integral = integral + setoff;        
+      float derivative = setoff - lasterror;
+      float correction = (kp*setoff + ki*integral + kd*derivative)*2;
+
+      move(right, left, speed, correction);
+         
+      lasterror = setoff;
+}
+
