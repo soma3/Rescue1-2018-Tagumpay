8db9ee180c5b4c01961aea53b5a9aba4d372d314
ShanylOng <38065130+ShanylOng@users.noreply.github.com>
2018-04-07 10:00:13 +0800

    Add files via upload

---
+++ GTS/GTS.cpp
@@ -0,0 +1,73 @@
+#include <GTS.h>
+#include <motor.h>
+#include <Wire.h>
+#include <VL53L0X.h>
+#include <helper_3dmath.h>
+#include <MPU6050_6Axis_MotionApps20.h>
+#include <I2Cdev.h>
+
+void GTS::gyroturn(int nextpos)
+{
+  void loop() {
+    int _currentpos = ((int)mpu.getangle() + 7200)%360;
+    int _nextpos = nextpos
+    int _adjust = _nextpos - _currentpos;
+      if (_adjust > 0){
+        if (_adjust < 180){
+          turnright(right,left,70);
+        }
+        if (_adjust > 180){
+          turnleft(right,left,70);
+        }
+        else {
+        }
+      }
+      if (_adjust < 0){
+        if (_adjust > -180){
+          turnleft(right,left,70);
+        }
+        if (_adjust < -180){
+          turnright(right,left,70);
+        }
+        else {
+        }
+      }
+      else {
+      }
+  }
+}
+
+void GTS::gyrostraight(float desipos)
+{
+ MPU6050 gyro;
+ float _origin = 0;
+ float _integral = 0;
+ float _lasterror = 0;
+
+ int _speed = 50;
+ void setup() {
+  move(right,left,255);
+  delay(2000);
+  Wire.begin();
+  gyro.start(-52,93,-1,985);
+ }
+ void loop() {
+  float _cfac = gyro.getangle();
+  float _setoff = _origin - _cfac;
+
+  float _integral = _integral + _setoff;
+  float _derivative = _setoff - _lasterror;
+  float _correction = (_kp*_setoff + _ki*_integral + _kd*_derivative)*2;
+
+  move(right, left, _speed, correction);
+
+  _lasterror = _setoff  
+ }
+}
+
+GTS::pid(float kp, float ki, float kd)
+{
+ float _kp = kp
+ float _ki = ki
+ float _kd = kd 
+}
---
+++ GTS/GTS.h
@@ -0,0 +1,29 @@
+#ifndef GTS_h
+#define GTS_h
+
+#include "Arduino.h"
+
+class GTS
+{
+  public:
+    void gyroturn(int nextpos);
+    void gyrostraight(float desipos);
+    pid(float kp, float ki, float kd);
+  private:
+    int _currentpos;
+    int _nextpos;
+    int _adjust;
+    float _origin;
+    float _integral;
+    float _lasterror;
+    int _speed;
+    float _cfac;
+    float _setoff;
+    float _derivative;
+    float _correction;
+    float _kp;
+    float _ki;
+    float _kd;
+};
+
+#endif
