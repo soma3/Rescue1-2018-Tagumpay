41a97707205b517a732502b7f4b9fb673123445e
zhizhizhizhizhi <zhizhizhizhizhi>
2018-04-14 21:31:53 +0800

    evac zone raspi test code

--- EvacZoneRaspiTest.py
+++ EvacZoneRaspiTest.py
@@ -7,29 +7,44 @@ import time
 import serial
 import struct
 
-ser = serial.Serial(port='/dev/ttyACM0',baudrate=9600)
+#ser = serial.Serial(port='/dev/ttyACM0',baudrate=9600)
 camera = PiCamera()
 camera.framerate = 30
 camera.sensor_mode = 2 #full pov
-camera.resolution = (656, 452)
-rawCapture = PiRGBArray(camera, size=(656, 452))
+camera.resolution = (640, 480)
+rawCapture = PiRGBArray(camera, size=(640, 480))
 
+global evaczone
 evaczone = 0
-threshold = 1.4
+threshold = 4
+thresholdwithcircles = 1.4
 minheight = 50
 
-def evaczonedetection(img):
+def evaczonedetection(img,evaczone):
+    evaczone = 0
     output = img.copy()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray,(5,5),0)
     ret, bin = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY_INV)
     image, contours, h = cv2.findContours(bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    circles = cv2.HoughCircles(bin, cv2.HOUGH_GRADIENT, 1.2, 50, param1=80, param2=80, minRadius=15)
+    if circles is not None:
+      circles = np.round(circles[0, :]).astype("int")
+      for (x,y,r) in circles:
+        if ((x<x2)or(x>x2+w)) and ((y<y2)or(y>y2+h)):
+            evaczone=1
+        cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+        cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+        print "start"
+        print x
+        print y
+        print r
     for cnt in contours:
         x2,y2,w,h = cv2.boundingRect(cnt)
-        ratio = float(h)/float(w) #w/h if camera rotated
-        if (ratio>threshold) and (h>minheight):
+        ratio = float(w)/float(h) #w/h if camera rotated
+        if (ratio>threshold) and (w>minheight): #w>minheight if camera rotated
             evaczone=1
-        ser.write(struct.pack('b',evaczone)
+#        ser.write(struct.pack('b',evaczone)
         cv2.rectangle(output, (x2,y2), (x2+w,y2+h), (255,0,0),2) #for debugging
         print "start" #for debugging
         print w #for debugging
@@ -37,29 +52,29 @@ def evaczonedetection(img):
         print ratio #for debugging
     cv2.imshow("output", output) #for debugging
     cv2.imshow("bin", bin) #for debugging
+    if (evaczone): #f
+      cv2.putText(output, "rectangle", (10,50), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, (0,255,0)) #f
+    else: #f
+      cv2.putText(output, "nothing", (10,50), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, (0,255,0)) #f
+    cv2.imshow("output", output) #f
+    cv2.imshow("bin", bin) #f
     return evaczone
 #time.sleep(0.1)
 
 for frame in camera.capture_continuous(rawCapture, format="bgr",  use_video_port=True):
-    if ser.read()==1 and evaczone != 1:
+#    if ser.read()==1 and evaczone != 1:
 #      evaczone=0
-      img=np.asarray(frame.array)
-      img = imutils.resize(img,300)
-      img = imutils.rotate_bound(img,90)
-    
-      evaczonedetection(img)
-    
-      print evaczone #f
-      if (evaczone): #f
-          cv2.putText(output, "rectangle", (10,50), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, (0,255,0)) #f
-      else: #f
-          cv2.putText(output, "nothing", (10,50), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, (0,255,0)) #f
-      cv2.imshow("output", output) #f
-      cv2.imshow("bin", bin) #f
-    key = cv2.waitKey(1) & 0xFF
-    rawCapture.truncate(0)
-    if key == ord("q"):
-      break
+  img=np.asarray(frame.array)
+  img = imutils.resize(img,300)
+  img = imutils.rotate_bound(img,90)
+
+  evaczone = evaczonedetection(img,evaczone)
+
+  print evaczone #f
+  key = cv2.waitKey(1) & 0xFF
+  rawCapture.truncate(0)
+  if key == ord("q"):
+    break
 
 
 
